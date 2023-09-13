09c4d0f545331f17188061c5e8a759783e7b0169
Tiago Silva <tiagobsb84@gmail.com>
2023-09-13 09:47:55 -0300

    adicionando rede sociais rodape

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -1,14 +1,14 @@
 import "./Rodape.css";
 
-import facebook from "../../assets/facebook.svg";
 import linkedin from "../../assets/linkedin.svg";
+import github from "../../assets/github.svg";
 
 const Rodape = () => {
     return(
         <section className="section-rodape">
             <div className="box-network">
-                <img src={facebook} alt="icone facebook" />
-                <img src={linkedin} alt="icone linkedin" />
+                <a className="link-network" href="https://www.linkedin.com/in/tiago-silva-b11350197/" target="_blank" rel="noreferrer"><img className="image-network" src={linkedin} alt="icone linkedin" /></a>
+                <a className="link-network" href="https://github.com/tiagobsb84" target="_blank" rel="noreferrer"><img className="image-network" src={github} alt="icone github" /></a>
             </div>
             <div>
                <p>Desenvolvido por Tiago Silva</p><p>&copy;{new Date().getFullYear()}</p> 
